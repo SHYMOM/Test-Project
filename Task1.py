176b3a3067fb17bb94078acf9e94821f4dfe9910
SHEIKH SHYMOM ISLAM <www.shymom124@gmail.com>
2026-02-22 20:15:16 -0800

    Hello

---
+++ Task1.py
@@ -0,0 +1,7 @@
+#Write a Python program to determine wether a given number is even or odd.
+
+number = int(input("Enter a number: "))
+if (number % 2) == 0:
+    print("The Number is Even")
+else:
+    print("The Number is Odd")
